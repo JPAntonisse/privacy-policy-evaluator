7aae556c3c04f4dba837ac36b0eaa2d5e7b74c6c
Joey Antonisse <j.p.antonisse@live.nl>
2019-11-01 15:46:41 +0100

    Updated env

--- ppe.py
+++ ppe.py
@@ -1,5 +1,16 @@
 from privacy_policy_evaluator import commands
 
+from nltk.tokenize import sent_tokenize, word_tokenize
+import matplotlib.pyplot as plt
+from collections import Counter
+import matplotlib.pyplot as plt
+from nltk.util import ngrams
+import matplotlib as mpl
+import numpy as np
+import nltk
+
+# settings
+plt.figure(figsize=(15, 15))
 
 def main(args=None):
     args = commands.parser.parse_args(args)
