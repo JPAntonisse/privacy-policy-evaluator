d5063ca5a3541a970d88781e9766cf03294d2662
Wesley <w.seubring@gmail.com>
2019-11-07 14:38:38 +0100

    Topic grouper

--- privacy_policy_evaluator/TFIDF.py
+++ privacy_policy_evaluator/TFIDF.py
@@ -4,4 +4,4 @@ def calculate_vectors(text, ngram_min = 1, ngram_max = 1):
     ngram_range = (ngram_min, ngram_max)
     tfidf = TfidfVectorizer(ngram_range= ngram_range)
     vecs = tfidf.fit_transform(text)
-    return vecs
+    return [vecs, tfidf]
