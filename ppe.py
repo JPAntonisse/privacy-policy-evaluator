a5cd7b6d8df2b3346068927facd47ae6ae117d9a
Joey Antonisse <j.p.antonisse@live.nl>
2019-11-07 11:50:45 +0100

    aadded arguments parsing

--- ppe.py
+++ ppe.py
@@ -1,15 +1,20 @@
 from privacy_policy_evaluator import commands
+from typing import Callable
 
 
 def main(args=None):
+    # Parse input
     args = commands.parser.parse_args(args)
-    try:
-        func = args.func
-    except AttributeError:
-        pass
-    else:
-        func(args)
+    # Select the function in this document that is the first argument
+    arg_func: Callable = globals()[args.function]
+    # Call the funciton
+    arg_func()
+
+
+def difference():
+    # DO the difference
+    print('aa')
 
 
 if __name__ == '__main__':
-    main(['-h'])
+    main()
--- privacy_policy_evaluator/commands/parser.py
+++ privacy_policy_evaluator/commands/parser.py
@@ -1,10 +1,22 @@
 import argparse
 
-parser = argparse.ArgumentParser('python-boilerplate')
-parser.add_argument('--version', '-v', action='version',
-                    version='%(prog)s 1.0.0')
-subparsers = parser.add_subparsers(
-    title='subcommands',
-    description='valid subcommands',
-    help='sub-command help',
+parser = argparse.ArgumentParser(
+    description='Privacy Policy Evaluator',
 )
+parser.add_argument('--version', action='version', version='%(prog)s 1.0.0')
+
+subparsers = parser.add_subparsers(
+    title='Privacy Policy Evaluator',
+    description='Commands',
+    dest='function',
+    help='Description',
+)
+#
+# create the parser for the "a" command
+parser_difference = subparsers.add_parser('difference', help='Show the difference of two documents')
+parser_difference.add_argument('doc1', type=str, help='Base Document')
+parser_difference.add_argument('doc2', type=str, help='Document to compare with')
+
+# create the parser for the "b" command
+parser_b = subparsers.add_parser('b', help='b help')
+parser_b.add_argument('--baz', choices='XYZ', help='baz help')
